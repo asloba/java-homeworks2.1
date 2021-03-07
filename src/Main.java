d2a5927cca78169dae7721ed1b096678a5620e0a
asloba <aslobaa@mail.ru>
2021-03-07 23:31:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = amount + transfer;
+        System.out.println(total);
+    }
+}
